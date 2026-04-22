a8ba4fbd9e890203c928af2097a08dffb2ce4948
imperialdiva <makardemedec@gmail.com>
2026-04-23 00:01:49 +0300

    edit states and added new states

--- bot/states/profile.py
+++ bot/states/profile.py
@@ -3,5 +3,7 @@ from aiogram.fsm.state import StatesGroup, State
 class ProfileRegistration(StatesGroup):
     name = State()
     age = State()
+    city = State() 
+    bio = State()  
     gender = State()
     photo = State()
